7be76ff9c557ed00bdbf6fa0844e8732b83ae06c
Nataliia Hlushchuk <hlushchuk.nataliia@gmail.com>
2022-11-02 23:28:23 +0300

    HW-11(1)

--- src/index.js
+++ src/index.js
@@ -7,18 +7,18 @@ import createMarkUpPhoto from './createMarkUp';
 
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
-// const loadMore = document.querySelector('.load-more');
+const loadMore = document.querySelector('.load-more');
 
 form.addEventListener('submit', onSubmitForm);
-// loadMore.addEventListener('click', onLoadMore);
+loadMore.addEventListener('click', onLoadMore);
 let totalHits = 0;
 
 const newsApi = new NewsApi();
 let lightbox = new SimpleLightbox('.gallery a');
-console.log(newsApi);
+
 async function onSubmitForm(e) {
   e.preventDefault();
-  // loadMore.classList.add('is-hidden');
+  loadMore.classList.add('is-hidden');
   if (e.target.tagName !== 'FORM') return;
   clearPhotoList();
   newsApi.query = e.target.elements.searchQuery.value.trim();
@@ -38,31 +38,31 @@ async function onSubmitForm(e) {
     } else {
       createMarkUpPhoto(gallery, setPhotos);
       lightbox.refresh();
-      // loadMore.classList.remove('is-hidden');
+      loadMore.classList.remove('is-hidden');
     }
   } catch (error) {
     Notiflix.Notify.failure(`Request error`);
   }
 }
 
-// async function onLoadMore(e) {
-//   if (e.target.tagName !== 'BUTTON') return;
-//   try {
-//     let response = await newsApi.fetchPhoto();
-//     let setPhotos = response.data.hits;
-//     createMarkUpPhoto(gallery, setPhotos);
-//     lightbox.refresh();
-//     scrollTo();
-//     if ((newsApi.page - 1) * newsApi.per_page > response.data.totalHits) {
-//       loadMore.classList.add('is-hidden');
-//       Notiflix.Notify.info(
-//         "We're sorry, but you've reached the end of search results"
-//       );
-//     }
-//   } catch (error) {
-//     Notiflix.Notify.failure(`Request error`);
-//   }
-// }
+async function onLoadMore(e) {
+  if (e.target.tagName !== 'BUTTON') return;
+  try {
+    let response = await newsApi.fetchPhoto();
+    let setPhotos = response.data.hits;
+    createMarkUpPhoto(gallery, setPhotos);
+    lightbox.refresh();
+    scrollTo();
+    if ((newsApi.page - 1) * newsApi.per_page > response.data.totalHits) {
+      loadMore.classList.add('is-hidden');
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results"
+      );
+    }
+  } catch (error) {
+    Notiflix.Notify.failure(`Request error`);
+  }
+}
 
 function clearPhotoList() {
   gallery.innerHTML = '';
@@ -77,23 +77,29 @@ function scrollTo() {
     behavior: 'smooth',
   });
 }
-let isEnd = false;
-window.addEventListener('scroll', async () => {
-  const documentRect = document.documentElement.getBoundingClientRect();
-  if (documentRect.bottom < document.documentElement.clientHeight + 150) {
-    if ((newsApi.page - 1) * newsApi.per_page > totalHits) {
-      if (!isEnd) {
-        isEnd = true;
-        Notiflix.Notify.info(
-          "We're sorry, but you've reached the end of search results"
-        );
-      }
-      return;
-    }
-    let response = await newsApi.fetchPhoto();
-    console.log(response);
-    let setPhotos = response.data.hits;
-    createMarkUpPhoto(gallery, setPhotos);
-    lightbox.refresh();
-  }
-});
+// Scroll
+
+// let isEnd = false;
+// let isPromise = false;
+// window.addEventListener('scroll', async () => {
+//   const documentRect = document.documentElement.getBoundingClientRect();
+//   if (documentRect.bottom < document.documentElement.clientHeight + 150) {
+//     if ((newsApi.page - 1) * newsApi.per_page > totalHits) {
+//       if (!isEnd) {
+//         isEnd = true;
+//         Notiflix.Notify.info(
+//           "We're sorry, but you've reached the end of search results"
+//         );
+//       }
+//       return;
+//     }
+//     if (!isPromise) {
+//       isPromise = true;
+//       let response = await newsApi.fetchPhoto();
+//       isPromise = false;
+//       let setPhotos = response.data.hits;
+//       createMarkUpPhoto(gallery, setPhotos);
+//       lightbox.refresh();
+//     }
+//   }
+// });
